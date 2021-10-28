982a11366a60c0a38796b1c2348ac6896944abe0
João Pedro Ribeiro <contato.joaopribeiro@gmail.com>
2021-10-28 09:43:57 -0300

    Mudança de cores dos echos.
    Feito mudança para melhor visualização, corrigido também problema de semântica.

--- atualizacao_Tzdata.sh
+++ atualizacao_Tzdata.sh
@@ -4,43 +4,45 @@ TZDATA1=$(rpm -qa |grep tzdata-2) #Coletando a versão do tzdata do sistema
 TZDATA2=$(rpm -qa |grep tzdata-java) #Coletando a versão do java do tzdata
 TZDATAPENELOPE=$(/opt/jre1.8.0_25/bin/java -jar /tmp/tzupdater.jar -V |grep JRE) #Coletando a versão do tzdata da penelope
 
+tput setaf 4; echo -e "Iniciando processo de verificações e atualizações!\n"; tput sgr0
+
 #Verifica se a versão do tzdata do sistema do java é a mais recente, se for ele pula o processo, caso contrário, inicia o processo de atualização. 
 if [[ $TZDATA1 == "tzdata-2020d-1.el6.noarch" && $TZDATA2 == "tzdata-java-2020d-1.el6.noarch" ]]
 then
     
-    tput setaf 2; echo "Tzdata do TC já está atualizado!"; tput sgr0
+    tput setaf 2; echo -e "Tzdata do TC já está atualizado!\n"; tput sgr0
 
 else
 
-    tput setaf 2; echo "Tzdata do TC já está atualizado! Iniciando os processos."; tput sgr0
+    tput setaf 5; echo -e "Tzdata do TC está desatualizado! Iniciando os processos.\n"; tput sgr0
 
-    tput setaf 2; echo 'Desabilitando verificação de SSL do yum.conf...'; tput sgr0
+    tput setaf 6; echo -e 'Desabilitando verificação de SSL do yum.conf...\n'; tput sgr0
     echo "sslverify=false" >> /etc/yum.conf #desativação necessária, pois por conta da versão atual do centos não existem mais repositórios oficiais da RedHat e por conta disso retornava erro com o SSL ativo.
 
     #Verifica se existem os arquivos de atualização no sistema, se tiver pula o processo de download, caso contrário, faz o download. 
     if [[ -f tzdata-2020d-1.el6.noarch && -f tzdata-java-2020d-1.el6.noarch ]]
     then
 
-        tput setaf 2; echo "O arquivo de atualização já existe no sistema, pulando etapa de download."; tput sgr0
+        tput setaf 2; echo -e "O arquivo de atualização já existe no sistema, pulando etapa de download.\n"; tput sgr0
 
     else
 
-        tput setaf 2; echo 'Baixando arquivos necessários...'; tput sgr0
+        tput setaf 6; echo -e 'Baixando arquivos necessários...\n'; tput sgr0
         wget github.com/JoaoPedro-Ribeiro/updatetzdata/raw/main/tzdata-2020d-1.el6.noarch.rpm
         wget github.com/JoaoPedro-Ribeiro/updatetzdata/raw/main/tzdata-java-2020d-1.el6.noarch.rpm
 
     fi
 
-    tput setaf 2; echo 'Instalando atualizações...'; tput sgr0
+    tput setaf 5; echo -e 'Instalando atualizações...\n'; tput sgr0
     yum update tzdata-2020d-1.el6.noarch.rpm -y
     yum update tzdata-java-2020d-1.el6.noarch.rpm -y
    
-    tput setaf 2; echo 'Verificando a hora...'; tput sgr0
+    tput setaf 6; echo -e 'Verificando a hora...\n'; tput sgr0
     hwclock -w
     date
     hwclock
 
-    tput setaf 2; echo 'Confira a versão atual para validar a atualização...'; tput sgr0
+    tput setaf 6; echo -e 'Confira a versão atual para validar a atualização...\n'; tput sgr0
     rpm -qa |grep tzdata
 
     VERIFICACAOSISTEMA=$(rpm -qa |grep tzdata) #Coleta qual a versão atual do tzdata do TC
@@ -48,11 +50,12 @@ else
     if [[ $VERIFICACAOSISTEMA =~ "tzdata-2020" ]]; 
     then
 
-        tput setaf 2; echo "Atualização do sistema realizada com sucesso!"; tput sgr0
+        tput setaf 2; echo -e "Atualização do sistema realizada com sucesso!\n"; tput sgr0
     
     else
 
-        tput setaf 1; echo "Atualização do sistema falhou!"; tput sgr0
+        tput setaf 1; echo -e "Atualização do sistema falhou!\n"; tput sgr0
+
     fi
 
 fi
@@ -61,36 +64,36 @@ fi
 if [[ $TZDATAPENELOPE =~ "tzdata2021" ]]; 
 then
 
-   tput setaf 2; echo "Tzdata da Penelope já está atualizado!"; tput sgr0
+   tput setaf 2; echo -e "Tzdata da Penelope já está atualizado!\n"; tput sgr0
 
 else
 
-    tput setaf 2; echo "Tzdata da Penelope já está atualizado! Iniciando os processos."; tput sgr0
+    tput setaf 5; echo -e "Tzdata da Penelope está desatualizado! Iniciando os processos.\n"; tput sgr0
 
     #Verifica se existem os arquivos de atualização no sistema, se tiver pula o processo de download, caso contrário, faz o download. 
     if [[ -f "/tmp/tzupdater.jar" ]]
     then
 
-        tput setaf 2; echo "O arquivo de atualização já existe no sistema, pulando etapa de download."; tput sgr0
+        tput setaf 6; echo -e "O arquivo de atualização já existe no sistema, pulando etapa de download.\n"; tput sgr0
     
     else
 
-        tput setaf 2; echo 'O arquivo de atualização da penelope ainda não existe. Baixando arquivos necessários...'; tput sgr0
+        tput setaf 6; echo -e 'O arquivo de atualização da penelope ainda não existe. Baixando arquivos necessários...\n'; tput sgr0
         wget github.com/JoaoPedro-Ribeiro/updatetzdata/raw/main/tzupdater.jar
 
-        tput setaf 2; echo 'Mudandos os arquivos necessários de local...'; tput sgr0   
+        tput setaf 6; echo -e 'Mudandos os arquivos necessários de local...\n'; tput sgr0   
         mv tzupdater.jar /tmp
 
     fi
 
-    tput setaf 2; echo 'Instalando atualizacao da Penelope...'; tput sgr0
+    tput setaf 5; echo -e 'Instalando atualizacao da Penelope...\n'; tput sgr0
     /opt/jre1.8.0_25/bin/java -jar /tmp/tzupdater.jar -f
 
-    tput setaf 2; echo 'Reiniciando serviço da Penelope'; tput sgr0
+    tput setaf 6; echo -e 'Reiniciando serviço da Penelope\n'; tput sgr0
     initctl stop adapter
     initctl start adapter
 
-    tput setaf 2; echo 'Confira a versão atual para validar a atualização...'; tput sgr0
+    tput setaf 6; echo -e 'Confira a versão atual para validar a atualização...\n'; tput sgr0
     /opt/jre1.8.0_25/bin/java -jar /tmp/tzupdater.jar -V
 
     VERIFICACAOPENELOPE=$(/opt/jre1.8.0_25/bin/java -jar /tmp/tzupdater.jar -V |grep JRE) #Coleta qual a versão atual do tzdata da penelope
